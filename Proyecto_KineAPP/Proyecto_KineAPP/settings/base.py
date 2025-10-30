f01deb01ee5267ed3c6e9eafbe6c450b7bb3d543
gabrieltenorio16 <gabriel.tenorio@alumnos.ucn.cl>
2025-10-29 23:28:59 -0300

    3era Versión
    Se agrego la applicaciones de diagnostico_paciente , aunque falta revisar la implementación

--- Proyecto_KineAPP/Proyecto_KineAPP/settings/base.py
+++ Proyecto_KineAPP/Proyecto_KineAPP/settings/base.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # aplicaciones propias 
-    'applications.modulos',
+    'applications.usuario',
 ]
 
 MIDDLEWARE = [
--- Proyecto_KineAPP/applications/usuario/apps.py
+++ Proyecto_KineAPP/applications/usuario/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ModulosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'applications.modulos'
+    name = 'applications.usuario'
